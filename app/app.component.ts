768aa95025bc66583244be0c18cd5a60e02ab114
pyotr pypenko <ppypenko@student.neumont.edu>
2016-05-18 13:50:11 -0600

    Got startup

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'nu-app',
+    template: `
+    <div>
+        <h1>NU Angular 2 Demo</h1>
+    </div>
+     `
+})
+export class AppComponent {}
